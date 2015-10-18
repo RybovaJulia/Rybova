55ca43a738ca300f3d0fe78b05ce165296ee34ab
Rybova Julia <rybova.julia0908@gmail.com>
2015-10-18 15:31:27 +0300

    Homework2

---
+++ 2.summadootric.cpp
@@ -0,0 +1,17 @@
+#include <iostream>//2)Программа считает сумму введенных чисел до первого отрицательного//
+#include <clocale>
+#include <cmath>
+using std::cout;
+using std::cin;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a=0, sum=0 ;
+	while (a>=0)
+	{
+		cout << "Введите число: ";
+		cin >> a;
+		if (a>0) sum=sum+a;;
+	}
+	cout << "Сумма положительных чисел= " <<sum<<"\n"; 
+}
