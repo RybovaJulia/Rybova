5ab863409d54752ede1b4692d6c41551772ac038
Julia <rybova.julia0908@gmail.com>
2016-03-21 23:45:25 +0300

    Homework_35

--- Second_semester/3.Homework35.cpp
+++ Second_semester/3.Homework35.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <cstring>
 #include <clocale> 
+#include <fstream>
 
 using namespace std;
 const int n=900;
@@ -10,23 +11,29 @@ const int n=900;
 char* vvod_string (char * str)
 {
 	int d=0; 
-	cout<<"Выберите способ ввода строки:"<<endl<<"  1. Автоматически "<<endl<<"  2. Вручную "<<endl; 
+	cout<<"Выберите способ ввода строки:"<<endl<<"  1. Автоматически "<<endl<<"  2. Ввод с клавиатуры "<<endl<<"  3. Из текстового файла"<<endl;  
 	cin>>d; 
 	switch(d){ 
 	case 1: strcpy(str,"Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do:  once or twice she had peeped into the book her sister was reading");
 		cout<<str<<endl; 
 		break; 
-	default:
+	 case 2 :
 		cout << "Введите строку" << "\n";
 		cin.get();
 		cin.getline(str, n);
+		break;
+	 default: ifstream infile ("D:\\Alice.txt"); 
+ 	     while (!infile.eof()){ 
+		infile.getline(str, n); 
+ 	} 
+		break;
 	} 
 	return str; 
 } 
 
-
 void max_lengh(char *str,int n)
 {  
+	char *source = str; 
 	char *p = new char[n];
 	char *s = new char[n];
 	strcpy(s, str);
@@ -40,12 +47,34 @@ void max_lengh(char *str,int n)
 		pch = strtok(NULL, " ,.-;:");
 	}
 	cout << "Максимальная длина слова="<<maxlen<<endl<<"Слова максимальной длины:"<<endl;
+
+	bool max_len_before(false);
 	char * pch1 = strtok(s, " ,.-;:");
 	while (pch1 != NULL)
 	{
-		if (strlen(pch1)==maxlen)
+		size_t cur_len = strlen(pch1);
+		if( max_len_before && cur_len != maxlen) //не удаляем если слова max длины идут друг за другом
+		{
+			size_t end = strlen(source)-cur_len;
+			memmove(source-1, source + cur_len, end); //смещаем строку начиная с позиции след. за максимальным + токен 
+			source[end-1] = '\0';
+		}
+		else
+		{
+			source+=(cur_len+1); //увеличиваем указатель на длину слова с токеном
+		}
+
+		if (cur_len==maxlen)
+		{
 			cout<<pch1<<endl;
-		   pch1 = strtok(NULL, " ,.-;:");
+			max_len_before = true;
+		}
+		else
+		{
+			max_len_before = false;
+		}
+
+		pch1 = strtok(NULL, " ,.-;:");
 	}
 }
 
@@ -57,5 +86,6 @@ int main()
 	int i=0;
 	vvod_string(str); 
 	max_lengh(str,n);
+	cout << "Строка после удаления=" <<endl<<str<<endl;
 	return 0;
 }
